be986d0c6351db6a2118dac511d96a4b8dc874b3
KimJW <jiwhan1015@yonsei.ac.kr>
2023-02-28 01:14:22 +0900

    added padding on cnn

--- CNN_CIFAR10.py
+++ CNN_CIFAR10.py
@@ -4,8 +4,8 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms, datasets
 
-batch_size = 128
-epoch = 20
+batch_size = 256
+epoch = 30
 
 # Set Devices (M1/M2 mps, NVIDIA cuda:0, else cpu)
 device = None
@@ -54,14 +54,14 @@ def Loaders(datas=trainset.data, labels=trainset.targets, idx_num=40000):
     valid_ds = MyDataset(data=valid_datas, label=valid_labels)
 
     train_load = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
-    valid_load = DataLoader(valid_ds, batch_size=1, shuffle=False)
+    valid_load = DataLoader(valid_ds, batch_size=batch_size, shuffle=False)
 
     print("Data Loading Completed")
     return train_load, valid_load
 
 
 trainloader, validloader = Loaders()
-testloader = DataLoader(testset, batch_size=1, shuffle=False)
+testloader = DataLoader(testset, batch_size=batch_size, shuffle=False)
 
 
 class CNN(nn.Module):
@@ -69,23 +69,26 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.batch = 0
         self.layer = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5),
+            # batch x 3 x 32 x 32 -> batch x 64 x 32 x 32
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, padding=2),
             nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=5),
+            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=5, padding=2),
             nn.ReLU(),
+            # batch x 32 x 32 x 32 -> batch x 32 x 16 x 16
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(in_channels=32, out_channels=16, kernel_size=5),
+            nn.Conv2d(in_channels=32, out_channels=16, kernel_size=5, padding=2),
             nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=8, kernel_size=5, padding=2),
+            nn.ReLU(),
+            # batch x 8 x 16 x 16 -> batch x 8 x 8 x 8
             nn.MaxPool2d(2, 2),
         )
 
         # Fully Connected Layer
         self.fcLayer = nn.Sequential(
-            nn.Linear(16 * 4 * 4, 32),
+            nn.Linear(8 * 8 * 8, 64),
             nn.ReLU(),
-            nn.Linear(32, 16),
-            nn.ReLU(),
-            nn.Linear(16, 10)
+            nn.Linear(64, 10)
         )
 
     def forward(self, X):
@@ -103,7 +106,7 @@ class Train:
         self.lossF = nn.CrossEntropyLoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
 
-    def trainStep(self, image, label):
+    def trainStep(self, image, label) -> float:
         x = image.to(device)
         y = label.to(device)
         self.optimizer.zero_grad()
@@ -111,48 +114,45 @@ class Train:
         loss = self.lossF(output, y)
         loss.backward()
         self.optimizer.step()
+        return loss
 
 
 if __name__ == "__main__":
     print("CNN Test")
     print(device)
     model = CNN().to(device)
-    trainer = Train(2e-3, model)
+    trainer = Train(1.5e-3, model)
 
     # Training
     for i in range(epoch):
-        trainer.lr = trainer.lr / (1 + i * 0.1)
+        sum_loss = 0
         model.train()
-        for image, label in trainloader:
-            trainer.trainStep(image, label)
+        for idx, [image, label] in enumerate(trainloader):
+            sum_loss += trainer.trainStep(image, label)
+            if idx % 12 == 11:
+                print("epoch: {}, loss: {}".format(i + 1, sum_loss/12))
+                sum_loss = 0
         model.eval()
-        total = 0
         correct = 0
         for image, label in validloader:
-
             x = image.to(device)
             y = label.to(device)
             output = model.forward(x)
-            outidx = torch.argmax(output)
-            total += 1
-            if outidx == y:
-                correct += 1
-        print("epoch: {}, accuracy: {}".format(i + 1, 100*correct/total))
+            outidx = torch.argmax(output, dim=1)
+            correct += batch_size - torch.count_nonzero(outidx - y)
+        print("epoch: {}, accuracy: {}\n\n".format(i + 1, 100*correct/10000))
 
     with torch.no_grad():
-        total_test = 0
-        correct_test = 0
+        correct = 0
         for image, label in testloader:
             x = image.to(device)
             y = label.to(device)
-
             output = model.forward(x)
-            outidx = torch.argmax(output)
-            total_test += 1
-            if outidx == y:
-                correct_test += 1
+            outidx = torch.argmax(output, dim=1)
+            correct += batch_size - torch.count_nonzero(outidx - y)
+        print("Accuracy: ", 100 * correct / 10000)
+        print(correct)
 
-        print("Accuracy: ", 100*correct_test/total_test)
-        print(correct_test)
-        print(total_test)
+
+
 
