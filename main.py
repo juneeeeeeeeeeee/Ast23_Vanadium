f3f9ceee3905ffb4ecb8f82c94f6ccc4ae5f5598
KimJiWhan <jiwhan1015@yonsei.ac.kr>
2023-03-19 13:40:40 +0900

    23.03.19. Edited Get Datas

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
 
     model = CNN.CNN().to(device)
     trainer = Train_01.Trainer01(0.00305408365, model, device)
-    train_load, valid_load, test_load = getCIFAR10.getCIFAR10(40000, batch_size)
+    train_load, valid_load, test_load = getCIFAR10.getCIFAR10(49000, batch_size)
 
     for i in range(1, epoch + 1):
         train(train_load, i)
